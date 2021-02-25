f562a83509df8f3136406217e5f8f2ae43495a2f
Benzino <benasabir@gmail.com>
2021-02-25 17:16:52 -0500

    tests passed, all questions working, all answers to questions are now valid

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 // Accessing the database
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: 3300,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
